889191e9ef714606ca82071cdc7977661f0cebe4
vikashkumar01 <vikashkumar103210@gmail.com>
2022-06-23 21:02:50 +0530

    blog

--- index.js
+++ index.js
@@ -35,7 +35,13 @@ mongoose.connect(process.env.MONGO_URL,{
 .then(console.log("connected to mongoDB"))
 .catch((err)=>(console.log(err)));
 
-
+app.use("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+  })
 
 
 app.use('/api/auth', auth);
